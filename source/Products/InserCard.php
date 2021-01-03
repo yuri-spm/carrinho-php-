d0398241d2936e077c50746ab981ddaf3467b68e
Yuri Monte <yuri.spm@gmail.com>
2021-01-03 13:10:27 -0300

    Organizando os arquivos

--- source/Products/InserCard.php
+++ source/Products/InserCard.php
@@ -6,27 +6,40 @@ namespace Source\Models;
 use Source\Core\Connect;
 
 use Source\Core\Session;
-use Source\Products\Card;
-use Source\Products\Product;
+use Source\Model\Card;
+use Source\Models\Product;
 
 require __DIR__ ."/../autoload.php";
 
+/**Iniciando a sessão carrinho */
 $carrinho = new Session();
 
-
-
-
-    $produto = $carrinho->set($_POST['id'], 1);
-  
-    var_dump(
-      $produto->all(),
-    );
-
-
-
-
-
+/**Atribuindo itens ao carrinho */
+$produto = $carrinho->set($_POST['id'], 1);
+var_dump(
+  $produto->all(),
+);
 
 
 ?>
-
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title> Super Rede - Card </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<head>
+<body>
+    <div class="container">
+      <h3>Nº CARRINHO</h3>
+    </div>
+    <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Produto</th>
+        <th>Preço</th>
+        <th>Quantidade</th>
+        <th>Excluir</th>
+      </tr>
+      <tbody>
